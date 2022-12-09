101cd904b0d6ff7e17b94f61cb4c6868a44ce75b
zhangyihui <524595725@qq.com>
2022-12-09 17:44:35 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <!-- 路由的出口，用于设置挂载点，规则对应组件显示在这里 -->
+    <router-view />
+    <h1>黑马头条</h1>
   </div>
 </template>
 
+<script>
+export default {
+  // 给组件起的名字
+  name: 'App'
+}
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    open: true // 浏览器自动打开
+  },
+  lintOnSave: true // 打开eslint检查
 })
